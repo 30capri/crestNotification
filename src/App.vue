25c731e3eec35e51e39212d2db895b26147ebb07
30capri <53044279+30capri@users.noreply.github.com>
2021-04-21 12:55:03 +0530

    Add files via upload

--- src/App.vue
+++ src/App.vue
@@ -1,1 +1,31 @@
+<template>
+    <div id="app">
+        <div id="nav">
+            </div>
+        <router-view />
+    </div>
+</template>
 
+<script>
+// import Login from './components/Login';
+    export default {
+        name: 'App'
+    }
+        
+</script>
+
+<style>
+    body {
+        background-color: #F0F0F0;
+    }
+    h1 {
+        padding: 0;
+        margin-top: 0;
+    }
+    #app {
+        width: 100vw;
+        margin: auto;
+        height: 100vh;
+    display: flex;
+    }
+</style>
